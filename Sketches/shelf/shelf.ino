3353a316f1f3bf8e0313fd2546ca34b55764e1e0
Nick Alto <nick@artefactgroup.com>
2014-10-22 22:07:34 -0700

    Integrated chronodot to calculate real time without iOS sync, removing gmt offset calculations and starting to work on alarm logic.

--- Sketches/shelf/shelf.ino
+++ Sketches/shelf/shelf.ino
@@ -1,3 +1,5 @@
+#include <Wire.h>
+#include <RTClib.h>
 #include <Time.h>
 #include <TimeAlarms.h>
 #include <ble_shield.h>
@@ -7,12 +9,11 @@
 #include <Timer.h>
 
 String time;
-String gmtOffset;
 time_t currentTime = 0;
-int gmt = 0;
-int gmtOffsetSign = 1;
+time_t alarm = 0;
 boolean receivingTimeStamp = false;
-boolean receivingGMTOffset = false;
+DateTime rtc_time;
+time_t alarms[10];
 
 //******************************************************************************************
 //******************************************************************************************
@@ -59,7 +60,7 @@ void ble_loop() {
   while ( ble_available() ) {
    
     currentCommand = ble_read();
-    boolean available = !handleTimeStamp(currentCommand) && !handleGMTOffset(currentCommand);
+    boolean available = !handleTimeStamp(currentCommand) ;
     
     if ( available && ble_available() ) {
       // Otherwise we are looking for an on/off value
@@ -100,15 +101,28 @@ void ble_loop() {
 //******************************************************************************************
 #define TIME_MSG_LEN  11   // time sync to PC is HEADER followed by unix time_t as ten ascii digits
 #define TIME_HEADER  'T'   // Header tag for serial time sync message
-#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
+#define TIME_REQUEST  7    // ASCII bell character requests a time sync message
+RTC_DS1307 RTC;
 
 //#########################################################################################
 // Time Setup
-// Set sync provider / function to call when sync is required 
+// Set sync provider / function to call when sync is required
 //#########################################################################################
 
 void time_setup() {
-   setSyncProvider( time_request_sync ); 
+   setSyncProvider( time_request_sync );
+   
+  Wire.begin();
+  RTC.begin();
+ 
+  // This section grabs the current datetime and compares it to
+  // the compilation time.  If necessary, the RTC is updated.
+  rtc_time = RTC.now();
+  DateTime compiled = DateTime(__DATE__, __TIME__);
+  if (rtc_time.unixtime() < compiled.unixtime()) {
+    Serial.println("RTC is older than compile time! Updating");
+    RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
 }
 
 //#########################################################################################
@@ -117,16 +131,57 @@ void time_setup() {
 //#########################################################################################
 
 void time_loop() {
-  
+
   if(timeStatus()!= timeNotSet) {
-    digitalWrite(13,timeStatus() == timeSet); // on if synced, off if needs refresh  
-    time_digital_clock_display();  
+    digitalWrite(13,timeStatus() == timeSet); // on if synced, off if needs refresh
+    time_digital_clock_display();
   }
+  
+  rtc_time = RTC.now();
+  Serial.print("Current time: ");
+  Serial.print(rtc_time.year(), DEC);
+  Serial.print('/');
+  Serial.print(rtc_time.month(), DEC);
+  Serial.print('/');
+  Serial.print(rtc_time.day(), DEC);
+  Serial.print(' ');
+  Serial.print(rtc_time.hour(), DEC);
+  Serial.print(':');
+  Serial.print(rtc_time.minute(), DEC);
+  Serial.print(':');
+  Serial.print(rtc_time.second(), DEC);
+  Serial.println();
+ checkAlarm();
 }
+
+void checkAlarm() {
+ 
+ boolean is_alarm_today = alarm && rtc_time.year() >= year(alarm) && rtc_time.month() >= month(alarm) && rtc_time.day() >= day(alarm);
+ boolean trigger_alarm = is_alarm_today && rtc_time.hour() >= hour(alarm) && rtc_time.minute() >= minute(alarm) && rtc_time.second() >= second(alarm);
+ Serial.print("SENT FROM IPHONE!: ");
+ Serial.print(year(alarm));
+ Serial.print("/");
+ Serial.print(month(alarm));
+ Serial.print("/");
+ Serial.print(day(alarm));
+  Serial.print(" ");
+ Serial.print(hour(alarm));
+ Serial.print(":");
+ Serial.print(minute(alarm));
+  Serial.print(":");
+ Serial.print(second(alarm));
+ Serial.println("");
+ 
+ if ( trigger_alarm ) {
+   Serial.print("ALARM!!!!!!");
+   Serial.println();
+ }
+}
+
 
 //#########################################################################################
 // Handle Time Stamp
-// Checks to see if we are receiving a time stamp and uses it to set time. 
+// Checks to see if we are receiving a time stamp and uses it to set time.
 // Sends TIMESET when completed.
 //#########################################################################################
 
@@ -135,21 +190,23 @@ boolean handleTimeStamp(unsigned char currentCommand ) {
   //If a message starts with TxxxxxxxxT it is a timestamp
 
   if( currentCommand == 'T' ) {
-    
+
     if(receivingTimeStamp) {
       //If we recieve a 'T' while receivingTimeStamp it signals the end of the message
       receivingTimeStamp = !receivingTimeStamp;
-     
+
       // Now to convert that string to a time_t that we can actually use.
-      for(int i = 0; i < time.length(); i++){   
-        char c = time.charAt(i);          
-         if( c >= '0' && c <= '9'){   
-           currentTime = (10 * currentTime) + (c - '0') ; // convert digits to a number    
+      for(int i = 0; i < time.length(); i++){
+        char c = time.charAt(i);
+         if( c >= '0' && c <= '9'){
+           currentTime = (10 * currentTime) + (c - '0') ; // convert digits to a number
          }
       }
-    
+
       Serial.println(currentTime);
-      setTime(currentTime);
+      
+//      setTime(currentTime);
+      alarm = currentTime;
       ble_send_data("TIMESET");
 
     } else {
@@ -158,78 +215,21 @@ boolean handleTimeStamp(unsigned char currentCommand ) {
        currentTime = 0;
        receivingTimeStamp = !receivingTimeStamp;
     }
-    
+
     return true;
-    
+
   } else if ( receivingTimeStamp ) {
-    
+
     // If we are receiving store values in time string
     timeChar = (const char*)&currentCommand;
     time += atoi(timeChar);
-    
+
     return true;
-    
-  } 
-  
+
+  }
+
   return false;
 }
-
-
-//#########################################################################################
-// Handle Time Stamp
-// Checks to see if we are receiving a time stamp and uses it to set time. 
-// Sends TIMESET when completed.
-//#########################################################################################
-
-boolean handleGMTOffset(unsigned char currentCommand ) {
-  const char *timeChar;
-  //If a message starts with GxxG it is a GMT Offset
-
-  if( currentCommand == 'G' ) {
-    
-    if(receivingGMTOffset) {
-      //If we recieve a 'G' while receivingGMTOffset it signals the end of the message
-      receivingGMTOffset = !receivingGMTOffset;
-
-      // Now to convert that string to a time_t that we can actually use.
-      for(int i = 0; i < gmtOffset.length(); i++){   
-        char c = gmtOffset.charAt(i);   
-         if( c >= '0' && c <= '9'){   
-           gmt = (10 * gmt) + (c - '0') ; // convert digits to a number    
-         }
-      }
-      
-      gmt = gmt * gmtOffsetSign;
-      ble_send_data("GMTSET");
-
-    } else {
-      //Otherwise it signals the transmission of a new offset - start receiving.
-       gmt = 0;
-       gmtOffset = "";
-       receivingGMTOffset = !receivingGMTOffset;
-    }
-    
-    return true;
-    
-  } else if ( receivingGMTOffset ) {
-    
-    // If we are receiving store values in time string
-    if( currentCommand == '-' ) {
-      gmtOffsetSign = -1;
-    }
-    
-    if( currentCommand >= '0' && currentCommand <= '9') {   
-      timeChar = (const char*)&currentCommand;
-      gmtOffset += atoi(timeChar);
-    }
-    
-    return true;
-    
-  } 
-  
-  return false;
-}
-
 
 //#########################################################################################
 // Time Digital Clock Display
@@ -238,7 +238,7 @@ boolean handleGMTOffset(unsigned char currentCommand ) {
 
 void time_digital_clock_display(){
   // digital clock display of the time
-  Serial.print(hour() + gmt);
+  Serial.print(hour());
   time_print_digits(minute());
   time_print_digits(second());
   Serial.print(" ");
@@ -246,8 +246,8 @@ void time_digital_clock_display(){
   Serial.print(" ");
   Serial.print(month());
   Serial.print(" ");
-  Serial.print(year()); 
-  Serial.println(); 
+  Serial.print(year());
+  Serial.println();
 }
 
 //#########################################################################################
@@ -270,7 +270,7 @@ void time_print_digits(int digits){
 
 time_t time_request_sync()
 {
-  Serial.print(byte(TIME_REQUEST));  
+  Serial.print(byte(TIME_REQUEST));
   return 0; // the time will be sent later in response to serial mesg
 }
 
@@ -284,12 +284,13 @@ time_t time_request_sync()
 
 void setup() {
   Serial.begin(57600);
+//  ble.functionPointers[0] = &handleTimeStamp;
   time_setup();
   ble_setup();
 }
 
 void loop()
-{ 
+{
   time_loop();
   ble_loop();
 }
